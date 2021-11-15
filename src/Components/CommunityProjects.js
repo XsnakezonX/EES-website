68c4eb53491b62bb17ccfcdf97329c5d3e8b60bb
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2021-11-15 16:48:42 +0000

    more styledcomponents containers drafts

--- src/Components/CommunityProjects.js
+++ src/Components/CommunityProjects.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const ArticleContainer = styled.section`
+const CommunityContainer = styled.section`
 background-color: #d1d1d1;
 height: 100vh;
 background-size: cover;
@@ -11,10 +11,30 @@ align-items: center;
 justify-content: center;
 `;
 
+const ProjectsContainer = styled.section`
+background-color: #d1d1d1;
+height: 100vh;
+background-size: cover;
+display: flex !important;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`;
+
+
 function CommunityProjects() {
     return (
         <>
-           <h1>Community Projects</h1>
+           <h1>Community Projects Page</h1>
+
+           <ProjectsContainer>
+            <h1>Projects</h1>
+            </ProjectsContainer>
+
+            <CommunityContainer>
+            <h1>Community</h1>
+            </CommunityContainer>
+
         </>
     );
 }
