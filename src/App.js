313dc4fd4c162ecb29760efb27f5c170951b89bb
michaelcdixon <michaelmail7@gmail.com>
2021-11-17 15:09:39 +0000

    pull

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { Routes, Route } from "react-router";
-import Home from './Components/Home';
-import Blog from './Components/Blog';
-import CommunityProjects from './Components/CommunityProjects';
-import Navbar from './Components/Nav/Navbar';
-import RightNavbar from './Components/Nav/RightNavBar';
-
-
+import Home from "./Components/Home";
+import Blog from "./Components/Blog";
+import CommunityProjects from "./Components/CommunityProjects";
+import Navbar from "./Components/Nav/Navbar";
+import RightNavbar from "./Components/Nav/RightNavBar";
 
 function App() {
-  return (
-    <>
-      
-
-      <Navbar />
+	return (
+		<>
+			<Navbar />
 			<RightNavbar />
 
-      <Routes>
+			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route exact path="/blog" element={<Blog />} />
-        <Route exact path="/community" element={<CommunityProjects />} />
+				<Route
+					exact
+					path="/community"
+					element={<CommunityProjects />}
+				/>
 			</Routes>
-      
-     
-    </>
-  );
+		</>
+	);
 }
 
 export default App;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,88 +2,85 @@ import React from "react";
 import styled from "styled-components";
 
 const LandingContainer = styled.section`
-background-color: #d1d1d1;
-height: 100vh;
-background-size: cover;
-display: flex !important;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+	background-color: #d1d1d1;
+	height: 100vh;
+	background-size: cover;
+	display: flex !important;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 `;
 
-
 const VideoContainer = styled.section`
-background-color: #a2a2ab;
-height: 150vh;
-background-size: cover;
-display: flex !important;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+	background-color: #a2a2ab;
+	height: 150vh;
+	background-size: cover;
+	display: flex !important;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 `;
 
 const MapContainer = styled.section`
-background-color: #8f8fba;
-height: 100vh;
-background-size: cover;
-display: flex !important;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+	background-color: #8f8fba;
+	height: 100vh;
+	background-size: cover;
+	display: flex !important;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 `;
 
 const SponsorContainer = styled.section`
-background-color: #4f4f66;
-height: 100vh;
-background-size: cover;
-display: flex !important;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+	background-color: #4f4f66;
+	height: 100vh;
+	background-size: cover;
+	display: flex !important;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 `;
 
 const SocialsContainer = styled.section`
-background-color: #424263;
-height: 100vh;
-background-size: cover;
-display: flex !important;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+	background-color: #424263;
+	height: 100vh;
+	background-size: cover;
+	display: flex !important;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 `;
 
 function Home() {
-    return (
-        <>
-            <h1>Home Page</h1>
-            
-            <LandingContainer>
-            <h1>Landing</h1>
-            </LandingContainer>
+	return (
+		<>
+			<h1>Home Page</h1>
 
-            <VideoContainer>
-            <h1>Video</h1>
-            <video  autoPlay muted loop >
-                <source src="CoverVideo.mp4" />
-                This Browser does not support the video tag
-                </video>
-            </VideoContainer>
+			<LandingContainer>
+				<h1>Landing</h1>
+			</LandingContainer>
 
-            <MapContainer>
-            <h1>Map</h1>
-            </MapContainer>
+			<VideoContainer>
+				<h1>Video</h1>
+				<video autoPlay muted loop>
+					<source src="CoverVideo.mp4" />
+					This Browser does not support the video tag
+				</video>
+			</VideoContainer>
 
-            <SponsorContainer> 
-            <h1>Sponsors</h1>
-            </SponsorContainer>
+			<MapContainer>
+				<h1>Map</h1>
+			</MapContainer>
 
-            <SocialsContainer>
-            <h1>Socials</h1>
-            </SocialsContainer>
+			<SponsorContainer>
+				<h1>Sponsors</h1>
+			</SponsorContainer>
 
-           
-        </>
-    );
+			<SocialsContainer>
+				<h1>Socials</h1>
+			</SocialsContainer>
+		</>
+	);
 }
 
 export default Home;
