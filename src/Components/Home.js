c93bb1c3ea4d166e399820a6ed190cab0fd68555
Zach Dive <zjhd201@exeter.ac.uk>
2021-11-18 11:07:42 +0000

    snazzy

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -180,152 +180,7 @@ const Styles = styled.div`
 
 
 function Home() {
-<<<<<<< HEAD
-    return (
-        <>
-            
-            <h1>Home Page</h1>
-            
-            <LandingContainer>
-
-                 <LandingTitle>
-                    <h1>Totally Free</h1>
-                    <h1>Always Innovating.</h1>
-                </LandingTitle>
-
-            </LandingContainer>
-
-            <VideoContainer>
-
-                {/* Muted promotional video that plays on render and loops */}
-
-                <h1>Video</h1>
-                <video  autoPlay muted loop >
-                    <source src="/Videos/CoverVideo.mp4" />
-                    This Browser does not support the video tag
-                </video>
-
-            </VideoContainer>
-
-            <ProjectsContainer>
-
-                <SectionTitle>
-                    <h1>We Are Exeter Entrepreneurs</h1>
-                </SectionTitle>
-
-                <SectionText>
-                    <p>Join the largest society on campus now, free.</p>
-                </SectionText>
-
-                    <button>Join our community</button>
-                
-
-            </ProjectsContainer>
-
-            <ProjectsContainer>
-
-                <SectionTitle>
-                    <h1>Our Community Projects</h1>
-                </SectionTitle>
-
-                <SectionText>
-                    <p>Exeter Entrepreneurs are proud to have an exciting, diverse range of events running throughout the</p>   
-                    <p>year. Be sure to check out what’s coming up next as well as our past events here.Join the largest society on campus now, free.</p>
-                </SectionText>
-
-                <ProjectRow>
-                    <ProjectCard>
-                        <img src="/ProjectPlaceHolder.svg" alt="Project Thumbnail" />
-                        <SectionText>
-                            <p>Project</p> 
-                        </SectionText>
-                    </ProjectCard>
-
-                    <ProjectCard>
-                        <img src="/ProjectPlaceHolder.svg" alt="Project Thumbnail" />
-                        <SectionText>
-                            <p>Project</p> 
-                        </SectionText>
-                    </ProjectCard>
-                </ProjectRow>
-
-                <ProjectRow>
-                    <ProjectCard>
-                        <img src="/ProjectPlaceHolder.svg" alt="Project Thumbnail" />
-                        <SectionText>
-                            <p>Project</p> 
-                        </SectionText>
-                    </ProjectCard>
-
-                    <ProjectCard>
-                        <img src="/ProjectPlaceHolder.svg" alt="Project Thumbnail" />
-                        <SectionText>
-                            <p>Project</p> 
-                        </SectionText>
-                    </ProjectCard>
-                </ProjectRow>
-
-            </ProjectsContainer>
-           
-
-            <SponsorContainer> 
-
-                <SectionTitle>
-                    <h1>Meet our Sponsors</h1>
-                </SectionTitle>
-                
-                <SectionText>
-                    <p>Every member, every event, every pizza slice is free because of these guys.</p>
-                </SectionText>
-
-
-                {/* clickable image Hyperlinks to sponsors websites */}
-
-                
-                <SponsorIcons> 
-                        <a href="https://home.kpmg/uk/en/home.html" target="_blank">
-                            <img src="/KPMG.svg" alt="Partner logo" />
-                        </a>
-                    
-                
-                <a href="https://about.google/" target="_blank">Google</a>
-                <a href="https://about.google/" target="_blank">GitHub</a>
-                <a href="https://www.mozilla.org/en-US/" target="_blank">Mozilla</a>
-                <a href="https://business-school.exeter.ac.uk/" target="_blank">Exeter Business</a>
-                <a href="https://www.marshmclennan.com/" target="_blank">Marsh and McLennan Companies </a>
-                <a href="https://www.ashurst.com/" target="_blank">Ashurt</a>
-                <a href="https://www.oaknorth.co.uk/" target="_blank">Oaknorth Bank</a>
-                <a href="https://www.db.com/" target="_blank">Deutsche Bank</a>
-                <a href="https://aws.amazon.com/" target="_blank">Amazon AWS</a>
-                <a href="https://www.teachfirst.org.uk/" target="_blank">teach first</a>
-                <a href="https://www.altitudeads.com/" target="_blank">altitude ads</a>
-                <a href="https://www.billingbetter.co.uk/" target="_blank">billing better</a>
-                <a href="https://deliveroo.co.uk/" target="_blank">deliveroo</a>
-                <a href="https://www.buzzfeed.com/uk " target="_blank">buzzfeed</a>
-                <a href="https://webwisemedia.co.uk/" target="_blank">web wise media</a>
-
-                </SponsorIcons>
-            </SponsorContainer>
-
-            <MapContainer>
-
-                <SectionTitle>
-                    <h1>Map</h1>
-                </SectionTitle>
-            <Styles>
-            
-            <Row className="blue-background">
-               <iframe title = "map" src="https://snazzymaps.com/embed/353429" width="900px" height="300px" ></iframe>
-            </Row>
-        </Styles>
-
-            </MapContainer>
-            
-
-           
-        </>
-    );
-=======
+    
 	return (
 		<>
 
@@ -483,16 +338,23 @@ function Home() {
 				</SponsorIcons>
 			</SponsorContainer>
 
-			<MapContainer>
-				<SectionTitle>
-					<h1>Map</h1>
-				</SectionTitle>
-			</MapContainer>
+                <MapContainer>
+
+                <SectionTitle>
+                    <h1>Map</h1>
+                </SectionTitle>
+                <Styles>
+
+                <Row className="blue-background">
+                <iframe title = "map" src="https://snazzymaps.com/embed/353429" width="900px" height="300px" ></iframe>
+                </Row>
+                </Styles>
+
+                </MapContainer>
 
 			
 		</>
 	);
->>>>>>> 890ca6043884d51802eaf8991f3b843bf2267074
 }
 
 export default Home;
