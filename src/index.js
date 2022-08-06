17a955e7903fbb445e3a522988732bc2e6b767c3
XsnakezonX <72930866+XsnakezonX@users.noreply.github.com>
2022-08-07 00:34:44 +0100

    firebase integration

--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBbxdfTTnFCM8nmCPNwSvz8Hn-7zf-Wx_I",
+  authDomain: "entrepreneurs-official-22-23.firebaseapp.com",
+  projectId: "entrepreneurs-official-22-23",
+  storageBucket: "entrepreneurs-official-22-23.appspot.com",
+  messagingSenderId: "113772020958",
+  appId: "1:113772020958:web:304f03b6d37fe2468c6e4f",
+  measurementId: "G-CEB3H1KH18"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
